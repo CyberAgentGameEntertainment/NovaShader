a10ebf11ec35225151284ce81956dc87efe37a4a
Haruki Yano <haruki.y.11g@gmail.com>
2023-01-06 16:59:58 +0900

    Add comments.

--- Assets/Nova/Runtime/Core/Shaders/ParticlesUberUnlitForward.hlsl
+++ Assets/Nova/Runtime/Core/Shaders/ParticlesUberUnlitForward.hlsl
@@ -2,7 +2,7 @@
 #define NOVA_PARTICLESUBERUNLITFORWARD_INCLUDED
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-#ifdef DEBUG_DISPLAY
+#ifdef DEBUG_DISPLAY // for backward compatibility
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Unlit.hlsl"
 #endif
 #include "ParticlesUberUnlit.hlsl"
@@ -36,7 +36,7 @@ half4 frag(Varyings input) : SV_Target
     InitializeInputData(inputData, input);
 
     half4 color = fragUnlit(input, true, true);
-    #ifdef DEBUG_DISPLAY
+    #ifdef DEBUG_DISPLAY  // for backward compatibility
     color = UniversalFragmentUnlit(inputData, color.rgb, color.a);
     #endif
     color.rgb = MixFog(color.rgb, inputData.fogCoord);
