6f7750b048f06c8913c1134bfa115dd32d4451a7
Haruki Yano <haruki.y.11g@gmail.com>
2023-05-26 22:23:44 +0900

    Add namespace

--- Assets/Demo/Demo03/Scripts/Demo03.cs
+++ Assets/Demo/Demo03/Scripts/Demo03.cs
@@ -1,45 +1,45 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class Demo03 : MonoBehaviour
+namespace Demo.Demo03.Scripts
 {
-    // Start is called before the first frame update
-    [SerializeField] private GameObject fpsTextGameObject;
-    private Text _fpsText;
-    private int _frameCount =0;
-    private readonly List<float> _fpsCounts = new List<float>();
-    void Start()
+    public class Demo03 : MonoBehaviour
     {
-        Application.targetFrameRate = 120;
-        _fpsText = fpsTextGameObject.GetComponent<Text>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        _frameCount++;
-        if (_frameCount > 1000)
+        // Start is called before the first frame update
+        [SerializeField] private GameObject fpsTextGameObject;
+        private Text _fpsText;
+        private int _frameCount =0;
+        private readonly List<float> _fpsCounts = new List<float>();
+        void Start()
         {
-            float fpsAverage = 0.0f;
-            foreach (var fps in _fpsCounts)
+            Application.targetFrameRate = 120;
+            _fpsText = fpsTextGameObject.GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _frameCount++;
+            if (_frameCount > 1000)
             {
-                fpsAverage += fps;
+                float fpsAverage = 0.0f;
+                foreach (var fps in _fpsCounts)
+                {
+                    fpsAverage += fps;
+                }
+
+                fpsAverage /= _fpsCounts.Count;
+                _fpsText.text = $"FPS(Average) = {fpsAverage:0.00}";
+            }
+            else
+            {
+                var fps = 1.0f / Time.deltaTime;
+                _fpsText.text = $"FPS = {fps:0.00}";
+                _fpsCounts.Add(fps);
             }
 
-            fpsAverage /= _fpsCounts.Count;
-            _fpsText.text = $"FPS(Average) = {fpsAverage:0.00}";
-        }
-        else
-        {
-            var fps = 1.0f / Time.deltaTime;
-            _fpsText.text = $"FPS = {fps:0.00}";
-            _fpsCounts.Add(fps);
-        }
 
-
+        }
     }
 }
