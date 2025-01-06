ccdfa7e632d41c23e1cd0064f1caa51c440e0495
Takayuki Kiyohara <kiyohara_takayuki@cyberagent.co.jp>
2025-01-06 11:21:19 +0900

    不具合検証用のスクリプトを追加

---
+++ Assets/test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Test_Vertex_Deformation");
+        }
+    }
+}
