c9ecbd639a987b6f2767a3495af6add82ae898c5
CA-Tatami <tatami_yuki@cyberagent.co.jp>
2025-02-20 15:21:34 +0900

    Material内の不要なテクスチャへの参照を取り除くスクリプトの仮ファイル上げ

---
+++ Assets/Nova/Editor/Foundation/Scripts/UnusedReferencesRemove.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------
+// Copyright 2025 CyberAgent, Inc.
+// --------------------------------------------------------------
+
+using UnityEditor;
+
+namespace Nova.Editor.Foundation.Scripts
+{
+    internal static class UnusedReferencesRemove
+    {
+        [MenuItem("Tools/NOVA Shader/RemoveUnusedReferences")]
+        private static void RemoveUnusedReferences()
+        {
+        }
+    }
+}
