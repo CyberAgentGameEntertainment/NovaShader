59032f1b2a82cb7b6d9c4d799dc41dac7656751d
畳 悠樹 <tatami_yuki@cyberagent.co.jp>
2025-02-26 17:57:41 +0900

    ParallaxMapとAlphaTransitionのテスト項目の追加

--- Assets/Tests/Editor/UnusedReferencesRemoverTest.cs
+++ Assets/Tests/Editor/UnusedReferencesRemoverTest.cs
@@ -51,6 +51,40 @@ namespace Tests.Editor
                     new Texture3D(1, 1, 1, TextureFormat.RGBA32, false));
                 material.SetFloat(MaterialPropertyNames.TintColorMode, (float)TintColorMode.SingleColor);
             }
+
+            // ParallaxMap
+            {
+                material.SetTexture(MaterialPropertyNames.ParallaxMap, new Texture2D(1, 1));
+                material.SetTexture(MaterialPropertyNames.ParallaxMap2DArray,
+                    new Texture2DArray(1, 1, 1, TextureFormat.RGBA32, false));
+                material.SetFloat(MaterialPropertyNames.ParallaxMapMode, (float)ParallaxMapMode.SingleTexture);
+            }
+
+            // ColorCorrection
+            {
+                material.SetTexture(MaterialPropertyNames.GradientMap, new Texture2D(1, 1));
+                material.SetFloat(MaterialPropertyNames.ColorCorrectionMode, (float)ColorCorrectionMode.None);
+            }
+
+            // AlphaTransition
+            {
+                material.SetTexture(MaterialPropertyNames.AlphaTransitionMap, new Texture2D(1, 1));
+                material.SetTexture(MaterialPropertyNames.AlphaTransitionMap2DArray,
+                    new Texture2DArray(1, 1, 1, TextureFormat.RGBA32, false));
+                material.SetFloat(MaterialPropertyNames.AlphaTransitionMapMode,
+                    (float)AlphaTransitionMapMode.SingleTexture);
+                material.SetFloat(MaterialPropertyNames.AlphaTransitionSecondTextureBlendMode,
+                    (float)AlphaTransitionBlendMode.None);
+            }
+
+            // Emission
+            {
+                material.SetTexture(MaterialPropertyNames.EmissionMap, new Texture2D(1, 1));
+                material.SetTexture(MaterialPropertyNames.EmissionMap2DArray,
+                    new Texture2DArray(1, 1, 1, TextureFormat.RGBA32, false));
+                material.SetFloat(MaterialPropertyNames.EmissionAreaType, (float)EmissionAreaType.ByTexture);
+                material.SetFloat(MaterialPropertyNames.EmissionMapMode, (float)EmissionMapMode.SingleTexture);
+            }
         }
 
         private static void CheckUnlitParams(Material material)
@@ -63,8 +97,34 @@ namespace Tests.Editor
 
             // TintColor
             {
-                Assert.IsNull(material.GetTexture(MaterialPropertyNames.TintMap));
                 Assert.IsNull(material.GetTexture(MaterialPropertyNames.TintMap3D));
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.TintMap));
+            }
+
+            // ParallaxMap
+            {
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.ParallaxMap2DArray));
+                Assert.IsNotNull(material.GetTexture(MaterialPropertyNames.ParallaxMap));
+            }
+
+            // ColorCorrection
+            {
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.GradientMap));
+            }
+
+            // AlphaTransition
+            {
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.AlphaTransitionMap2DArray));
+                Assert.IsNotNull(material.GetTexture(MaterialPropertyNames.AlphaTransitionMap));
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.AlphaTransitionMapSecondTexture));
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.AlphaTransitionMapSecondTexture2DArray));
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.AlphaTransitionMapSecondTexture3D));
+            }
+
+            // Emission
+            {
+                Assert.IsNull(material.GetTexture(MaterialPropertyNames.EmissionMap2DArray));
+                Assert.IsNotNull(material.GetTexture(MaterialPropertyNames.EmissionMap));
             }
         }
     }
