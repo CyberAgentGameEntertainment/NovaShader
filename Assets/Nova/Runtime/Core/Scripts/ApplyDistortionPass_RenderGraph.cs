a9010288b6256adea7c5744c4f368411645e307c
CA-Tatami <tatami_yuki@cyberagent.co.jp>
2025-02-27 19:14:21 +0900

    Global Texture指定処理途中まで

--- Assets/Nova/Runtime/Core/Scripts/ApplyDistortionPass_RenderGraph.cs
+++ Assets/Nova/Runtime/Core/Scripts/ApplyDistortionPass_RenderGraph.cs
@@ -3,12 +3,34 @@
 // --------------------------------------------------------------
 
 #if UNITY_2023_3_OR_NEWER
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.Universal;
 
 namespace Nova.Runtime.Core.Scripts
 {
     public partial class ApplyDistortionPass : ScriptableRenderPass
     {
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            using (var builder = renderGraph.AddUnsafePass<PassData>("NOVA.ApplyDistortionPass",
+                       out var passData))
+            {
+                // builder.SetRenderFunc(static (PassData data, UnsafeGraphContext context) =>
+                // {
+                //     var cmd = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
+                //     cmd.SetGlobalTexture(_mainTexPropertyId, source);
+                //     cmd.SetGlobalTexture(_distortionBufferPropertyId, _distortedUvBufferIdentifier);
+                //     Blitter.BlitCameraTexture(cmd, cocTex, cocTex,
+                //         RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store, material,
+                //         (int)Pass.CreateCocTexture);
+                // });
+            }
+        }
+
+        private class PassData
+        {
+        }
     }
 }
 #endif
