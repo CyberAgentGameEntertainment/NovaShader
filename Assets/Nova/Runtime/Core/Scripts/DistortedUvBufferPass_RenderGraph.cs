4c5efc11345be620c9621ba16399fabc6cbb330d
CA-Tatami <tatami_yuki@cyberagent.co.jp>
2025-02-27 18:25:27 +0900

    コメントの追加

--- Assets/Nova/Runtime/Core/Scripts/DistortedUvBufferPass_RenderGraph.cs
+++ Assets/Nova/Runtime/Core/Scripts/DistortedUvBufferPass_RenderGraph.cs
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------
-// Copyright 2024 CyberAgent, Inc.
+// Copyright 2025 CyberAgent, Inc.
 // --------------------------------------------------------------
 
 #if UNITY_2023_3_OR_NEWER
@@ -13,8 +13,8 @@ namespace Nova.Runtime.Core.Scripts
 {
     public partial class DistortedUvBufferPass : ScriptableRenderPass
     {
-        private TextureHandle _distortedUvBufferTHdl;
         private static readonly string DistortedUvBufferTexName = "DistortedUvBuffer";
+        private TextureHandle _distortedUvBufferTHdl;
 
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
         {
@@ -23,22 +23,24 @@ namespace Nova.Runtime.Core.Scripts
             {
                 var resourcesData = frameData.Get<UniversalResourceData>();
                 _distortedUvBufferTHdl = CreateRenderTargets(renderGraph, frameData);
-                builder.SetRenderAttachment(_distortedUvBufferTHdl, 0, AccessFlags.Write);
-                builder.SetRenderAttachmentDepth(resourcesData.activeDepthTexture, AccessFlags.Write);
+                builder.SetRenderAttachment(_distortedUvBufferTHdl, 0);
+                builder.SetRenderAttachmentDepth(resourcesData.activeDepthTexture);
+
+                // TODO: ApplyDistortionPass側でGlobalTextureを設定する
                 // builder.SetGlobalTextureAfterPass(_distortedUvBufferTHdl, TransparentTexturePropID);
-                
+
                 RendererListHandle renderList;
                 {
                     var renderingData = frameData.Get<UniversalRenderingData>();
                     var cameraData = frameData.Get<UniversalCameraData>();
                     var lightData = frameData.Get<UniversalLightData>();
 
-                    var param = InitRendererListParams(_shaderTagId, renderingData, cameraData, lightData, 
+                    var param = InitRendererListParams(_shaderTagId, renderingData, cameraData, lightData,
                         SortingCriteria.CommonTransparent, _filteringSettings);
                     renderList = renderGraph.CreateRendererList(param);
                 }
                 builder.UseRendererList(renderList);
-                
+
                 builder.SetRenderFunc((PassData data, RasterGraphContext context) =>
                 {
                     context.cmd.DrawRendererList(data.RendererList);
@@ -52,9 +54,7 @@ namespace Nova.Runtime.Core.Scripts
             var desc = renderGraph.GetTextureDesc(resourceData.activeColorTexture);
             desc.depthBufferBits = 0;
             if (SystemInfo.IsFormatSupported(GraphicsFormat.R8G8_UNorm, GraphicsFormatUsage.Render))
-            {
                 desc.colorFormat = GraphicsFormat.R8G8_UNorm;
-            }
             return renderGraph.CreateTexture(desc);
         }
 
@@ -66,11 +66,11 @@ namespace Nova.Runtime.Core.Scripts
             SortingCriteria sortingCriteria,
             FilteringSettings filteringSettings)
         {
-            var drawSettings = RenderingUtils.CreateDrawingSettings(shaderTagId, renderingData, cameraData, lightData, 
-                                                                    sortingCriteria);
+            var drawSettings = RenderingUtils.CreateDrawingSettings(shaderTagId, renderingData, cameraData, lightData,
+                sortingCriteria);
             return new RendererListParams(renderingData.cullResults, drawSettings, filteringSettings);
         }
-        
+
         private class PassData
         {
             internal Material Material;
