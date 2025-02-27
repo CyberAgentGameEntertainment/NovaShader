6e4d2e1c541b471ac89e351024f4ff0c36a5c311
CA-Tatami <tatami_yuki@cyberagent.co.jp>
2025-02-27 18:30:07 +0900

    ApplyDistortionPassのRender Graph版ファイルの仮作成

---
+++ Assets/Nova/Runtime/Core/Scripts/ApplyDistortionPass_RenderGraph.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------
+// Copyright 2025 CyberAgent, Inc.
+// --------------------------------------------------------------
+
+#if UNITY_2023_3_OR_NEWER
+using UnityEngine.Rendering.Universal;
+
+namespace Nova.Runtime.Core.Scripts
+{
+    public partial class ApplyDistortionPass : ScriptableRenderPass
+    {
+    }
+}
+#endif
